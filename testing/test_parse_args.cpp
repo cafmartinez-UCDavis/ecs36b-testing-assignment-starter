8a2c181296599a3f07b596a000c529f053fca0e7
Christian <cafmartinez@ucdavis.edu>
2026-04-18 00:00:04 -0700

    final

--- testing/test_parse_args.cpp
+++ testing/test_parse_args.cpp
@@ -1,5 +1,3 @@
-#if 0
-// everything in the file
 
 #include <cstdlib>
 #include <string>
@@ -9,29 +7,43 @@
 #include "rapidcheck/gtest.h"
 
 auto word_generator() {
-    /* Creates a generator for a string made up of
-     * characters from [a - z]
-     */
+  return rc::gen::container<std::string>(
+      rc::gen::inRange(1, 10),
+      rc::gen::inRange('a', 'z' + 1)
+  );
 }
 
 auto vector_of_ints_to_vector_of_strings(const std::vector<int>& numbers) {
-    /* Create a vector of strings from a vector of ints
-     */
+  std::vector<std::string> result;
+  result.reserve(numbers.size());
+  for (int n : numbers) {
+    result.push_back(std::to_string(n));
+  }
+  return result;
 }
 
 
 TEST(ParseArgsTests, SimpleCheckArgumentsParsedSuccessfully) {
-    /*
-     * Check that you parse the command line arguments correctly.
-     * (ar_out and len_out are set to the right values).
-     * Don't forget to free any memory that was dynamically allocated as part of your test.'
-     */
+  const char* argv[] = {"prog", "1", "2", "3"};
+  int argc = 4;
+
+  int* result = parse_args(argc, const_cast<char**>(argv));
+
+  ASSERT_NE(result, nullptr);
+  EXPECT_EQ(result[0], 1);
+  EXPECT_EQ(result[1], 2);
+  EXPECT_EQ(result[2], 3);
+
+  delete[] result;
 }
 
 TEST(ParseArgsTests, SimpleCheckParseNoArgs) {
-    /*
-     * Check that you parse you can successfully parse "no" command line arguments.
-     */
+  const char* argv[] = {"prog"};
+  int argc = 1;
+
+  int* result = parse_args(argc, const_cast<char**>(argv));
+
+  EXPECT_EQ(result, nullptr);
 }
 
 
@@ -39,19 +51,41 @@ RC_GTEST_PROP(ParseArgsTests,
               PropertyCheckArgumentsParsedSuccessfully,
               ()
 ) {
-    /* Check that we can correctly parse the command line
-     * arguments when we receive 1 or more arguments.
-     * Don't forget to free any memory that was dynamically allocated as part of this test
-     */
+  int len = *rc::gen::inRange(1, 10);
+  std::vector<int> values =
+      *rc::gen::container<std::vector<int>>(len, rc::gen::arbitrary<int>());
+
+  int argc = len + 1;
+  char** argv = new char*[argc];
+  argv[0] = const_cast<char*>("prog");
+
+  for (int i = 0; i < len; i++) {
+    std::string s = std::to_string(values[i]);
+    argv[i + 1] = strdup(s.c_str());
+  }
+
+  int* result = parse_args(argc, argv);
+
+  RC_ASSERT(result != nullptr);
+
+  for (int i = 0; i < len; i++) {
+    RC_ASSERT(result[i] == values[i]);
+  }
+
+  delete[] result;
+  for (int i = 1; i < argc; i++) free(argv[i]);
+  delete[] argv;
 }
 
 RC_GTEST_PROP(ParseArgsTests,
               PropertyCheckParseNoArgs,
               ()
 ) {
-    /*
-     * Check that you parse you can successfully parse "no" command line arguments.
-     */
+  const char* argv[] = {"prog"};
+  int argc = 1;
+
+  int* result = parse_args(argc, const_cast<char**>(argv));
+
+  RC_ASSERT(result == nullptr);
 }
 
-#endif
