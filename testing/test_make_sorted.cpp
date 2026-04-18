bde2bdb213c582f2d4e5941afe2a53b90aef71ea
Christian <cafmartinez@ucdavis.edu>
2026-04-17 23:13:03 -0700

    Finished test cases and Rapid test cases but haven't fixed the sorting file yet

--- testing/test_make_sorted.cpp
+++ testing/test_make_sorted.cpp
@@ -1,4 +1,3 @@
-#if 0
 
 #include <cstdlib>
 #include <vector>
@@ -12,6 +11,15 @@ TEST(MakeSortedTests, SimpleSortSortedArray) {
      * Check that we can sort an array that is already sorted.
      * Don't forget to free any memory that was dynamically allocated as part of your test.
      */
+    int ar[] = {1, 2, 3, 4, 5};
+    int len = 5;
+
+    make_sorted(ar, len);
+
+    int expected[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < len; i++) {
+      EXPECT_EQ(ar[i], expected[i]);
+    }
 }
 
 TEST(MakeSortedTests, SimpleSortReverseSortedArray) {
@@ -19,6 +27,15 @@ TEST(MakeSortedTests, SimpleSortReverseSortedArray) {
      * Check that we can sort an array that is reverse sorted order.
      * Don't forget to free any memory that was dynamically allocated as part of your test.
      */
+    int array[] = {9, 7, 5, 3, 1};
+    int len = 5;
+
+    make_sorted(array, len);
+
+    int expected[] = {1, 3, 5, 7, 9};
+    for (int i = 0; i < len; i++) {
+      EXPECT_EQ(array[i], expected[i]);
+    }
 }
 
 
@@ -27,6 +44,15 @@ TEST(MakeSortedTests, SimpleSortAverageArray) {
      * Check that we can sort an array where the elements in it are in random order.
      * Don't forget to free any memory that was dynamically allocated as part of your test.
      */
+    int array[] = {4, 1, 7, 3, 2};
+    int len = 5;
+
+    make_sorted(array, len);
+
+    int expected[] = {1, 2, 3, 4, 7};
+    for (int i = 0; i < len; i++) {
+        EXPECT_EQ(array[i], expected[i]);
+    }
 }
 
 TEST(MakeSortedTests, SimpleSortArrayWithDuplicates) {
@@ -34,6 +60,15 @@ TEST(MakeSortedTests, SimpleSortArrayWithDuplicates) {
      * Check that we can sort an array where there are duplicate elements in it.
      * Don't forget to free any memory that was dynamically allocated as part of your test.
      */
+    int array[] = {5, 2, 5, 1, 2};
+    int len = 5;
+
+    make_sorted(array, len);
+
+    int expected[] = {1, 2, 2, 5, 5};
+    for (int i = 0; i < len; i++) {
+        EXPECT_EQ(array[i], expected[i]);
+    }
 }
 
 RC_GTEST_PROP(MakeSortedTests,
@@ -43,7 +78,20 @@ RC_GTEST_PROP(MakeSortedTests,
     /* Test that after sorting an array, the values are in ascending order
      * Don't forget to free any memory that was dynamically allocated as part of your test.
      */
+    RC_PRE(!values.empty());
+
+    int len = values.size();
+    int* ar = new int[len];
+
+    for (int i = 0; i < len; i++) {
+        ar[i] = values[i];
+    }
+
+    make_sorted(ar, len);
+
+    for (int i = 0; i < len - 1; i++) {
+        RC_ASSERT(ar[i] <= ar[i + 1]);
+    }
+
+    delete[] ar;
 }
-
-
-#endif
